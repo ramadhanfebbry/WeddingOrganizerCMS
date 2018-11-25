8a791323c37e61beb65194454c97841813e5e93a
febbry ramadhan <ramadhanfebbry@gmail.com>
2018-11-25 07:30:50 +0700

    update

---
+++ admin/barang_sewa/index.php
@@ -0,0 +1,360 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="icon" href="../../img/favicon.png" type="image/png">
+  <title>Real State Multi</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="../../css/bootstrap.css">
+  <link rel="stylesheet" href="../../vendors/linericon/style.css">
+  <link rel="stylesheet" href="../../css/font-awesome.min.css">
+  <link rel="stylesheet" href="../../vendors/owl-carousel/owl.carousel.min.css">
+  <link rel="stylesheet" href="../../vendors/lightbox/simpleLightbox.css">
+  <link rel="stylesheet" href="../../vendors/nice-select/css/nice-select.css">
+  <link rel="stylesheet" href="../../vendors/animate-css/animate.css">
+  <link rel="stylesheet" href="../../vendors/jquery-ui/jquery-ui.css">
+  <!-- main css -->
+  <link rel="stylesheet" href="../../css/style.css">
+  <link rel="stylesheet" href="../../css/responsive.css">
+  <link rel="stylesheet" href="../../css/custom.css">
+
+</head>
+<body>
+
+  <!--================Header Menu Area =================-->
+  <header class="header_area">
+    <div class="main_menu">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container box_1620">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <a class="navbar-brand logo_h" href="index.html"><img src="../../img/logo.png" alt=""></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+            <ul class="nav navbar-nav menu_nav ml-auto">
+              <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li> 
+              <li class="nav-item"><a class="nav-link" href="about-us.html">About</a></li>
+              <li class="nav-item"><a class="nav-link" href="properties.html">Properties</a></li>
+              <li class="nav-item"><a class="nav-link" href="agents.html">Team</a></li>
+              <li class="nav-item submenu dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages</a>
+                <ul class="dropdown-menu">
+                  <li class="nav-item"><a class="nav-link" href="elements.html">Elements</a></li>
+                </ul>
+              </li> 
+              <li class="nav-item submenu dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog</a>
+                <ul class="dropdown-menu">
+                  <li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
+                  <li class="nav-item"><a class="nav-link" href="single-blog.html">Blog Details</a></li>
+                </ul>
+              </li> 
+              <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li class="nav-item"><a href="#" class="search"><i class="lnr lnr-magnifier"></i></a></li>
+            </ul>
+          </div> 
+        </div>
+      </nav>
+    </div>
+  </header>
+  <!--================Header Menu Area =================-->
+
+  <!--================Home Banner Area =================-->
+  <section class="banner_area">
+    <div class="banner_inner d-flex align-items-center">
+      <div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background="" style="transform: translateY(-50px);"></div>
+      <div class="container">
+        <div class="banner_content">
+          <div class="page_link">
+            <a href="index.html">Home</a>
+            <a href="contact.html">Agents</a>
+          </div>
+          <h2>Agents</h2>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--================End Home Banner Area =================-->
+
+  <!--================Welcome Area =================-->
+
+  <!--================End Welcome Area =================-->
+
+  <!--================Properties Area =================-->
+  <section class="properties_area p_120">
+    <div class="container">
+      <div class="table-wrapper">
+        <div class="table-title">
+          <div class="row">
+            <div class="col-sm-6">
+              <h2>Manage <b>Barang</b></h2>
+            </div>
+            <div class="col-sm-6">
+              <a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="fa fa-plus-circle"></i> <span>Add New Employee</span></a>
+              <a href="#deleteEmployeeModal" class="btn btn-danger" data-toggle="modal"><i class="fa fa-trash-o"></i> <span>Delete</span></a>            
+            </div>
+          </div>
+        </div>
+        <table class="table table-striped table-hover">
+          <thead>
+            <tr>
+              <th>
+                <span class="custom-checkbox">
+                  <input type="checkbox" id="selectAll">
+                  <label for="selectAll"></label>
+                </span>
+              </th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Address</th>
+              <th>Phone</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <span class="custom-checkbox">
+                  <input type="checkbox" id="checkbox1" name="options[]" value="1">
+                  <label for="checkbox1"></label>
+                </span>
+              </td>
+              <td>Thomas Hardy</td>
+              <td>thomashardy@mail.com</td>
+              <td>89 Chiaroscuro Rd, Portland, USA</td>
+              <td>(171) 555-2222</td>
+              <td>
+                <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="fa fa-pencil" data-toggle="tooltip" title="Edit"></i></a>
+                <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="fa fa-trash" data-toggle="tooltip" title="Delete"></i></a>
+              </td>
+            </tr>
+
+          </tbody>
+        </table>
+        <div class="clearfix">
+          <div class="hint-text">Showing <b>5</b> out of <b>25</b> entries</div>
+          <ul class="pagination">
+            <li class="page-item disabled"><a href="#">Previous</a></li>
+            <li class="page-item"><a href="#" class="page-link">1</a></li>
+            <li class="page-item"><a href="#" class="page-link">2</a></li>
+            <li class="page-item active"><a href="#" class="page-link">3</a></li>
+            <li class="page-item"><a href="#" class="page-link">4</a></li>
+            <li class="page-item"><a href="#" class="page-link">5</a></li>
+            <li class="page-item"><a href="#" class="page-link">Next</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- Edit Modal HTML -->
+    <div id="addEmployeeModal" class="modal fade">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <form>
+            <div class="modal-header">            
+              <h4 class="modal-title">Add Employee</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            </div>
+            <div class="modal-body">          
+              <div class="form-group">
+                <label>Name</label>
+                <input type="text" class="form-control" required>
+              </div>
+              <div class="form-group">
+                <label>Email</label>
+                <input type="email" class="form-control" required>
+              </div>
+              <div class="form-group">
+                <label>Address</label>
+                <textarea class="form-control" required></textarea>
+              </div>
+              <div class="form-group">
+                <label>Phone</label>
+                <input type="text" class="form-control" required>
+              </div>          
+            </div>
+            <div class="modal-footer">
+              <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
+              <input type="submit" class="btn btn-success" value="Add">
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <!-- Edit Modal HTML -->
+    <div id="editEmployeeModal" class="modal fade">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <form>
+            <div class="modal-header">            
+              <h4 class="modal-title">Edit Employee</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            </div>
+            <div class="modal-body">          
+              <div class="form-group">
+                <label>Name</label>
+                <input type="text" class="form-control" required>
+              </div>
+              <div class="form-group">
+                <label>Email</label>
+                <input type="email" class="form-control" required>
+              </div>
+              <div class="form-group">
+                <label>Address</label>
+                <textarea class="form-control" required></textarea>
+              </div>
+              <div class="form-group">
+                <label>Phone</label>
+                <input type="text" class="form-control" required>
+              </div>          
+            </div>
+            <div class="modal-footer">
+              <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
+              <input type="submit" class="btn btn-info" value="Save">
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <!-- Delete Modal HTML -->
+    <div id="deleteEmployeeModal" class="modal fade">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <form>
+            <div class="modal-header">            
+              <h4 class="modal-title">Delete Employee</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            </div>
+            <div class="modal-body">          
+              <p>Are you sure you want to delete these Records?</p>
+              <p class="text-warning"><small>This action cannot be undone.</small></p>
+            </div>
+            <div class="modal-footer">
+              <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
+              <input type="submit" class="btn btn-danger" value="Delete">
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--================End Properties Area =================-->
+
+  <!--================End Clients Logo Area =================-->
+
+
+  <!--================ start footer Area  =================-->  
+  <footer class="footer-area p_120">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3  col-md-6 col-sm-6">
+          <div class="single-footer-widget">
+            <h6 class="footer_title">About Us</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6">
+          <div class="single-footer-widget">
+            <h6 class="footer_title">Newsletter</h6>
+            <p>Stay updated with our latest trends</p>    
+            <div id="mc_embed_signup">
+              <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscribe_form relative">
+                <div class="input-group d-flex flex-row">
+                  <input name="EMAIL" placeholder="Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email Address '" required="" type="email">
+                  <button class="btn sub-btn"><span class="lnr lnr-arrow-right"></span></button>    
+                </div>                  
+                <div class="mt-10 info"></div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6">
+          <div class="single-footer-widget instafeed">
+            <h6 class="footer_title">Instagram Feed</h6>
+            <ul class="list instafeed d-flex flex-wrap">
+              <li><img src="../../img/instagram/Image-01.jpg" alt=""></li>
+              <li><img src="../../img/instagram/Image-02.jpg" alt=""></li>
+              <li><img src="../../img/instagram/Image-03.jpg" alt=""></li>
+              <li><img src="../../img/instagram/Image-04.jpg" alt=""></li>
+              <li><img src="../../img/instagram/Image-05.jpg" alt=""></li>
+              <li><img src="../../img/instagram/Image-06.jpg" alt=""></li>
+              <li><img src="../../img/instagram/Image-07.jpg" alt=""></li>
+              <li><img src="../../img/instagram/Image-08.jpg" alt=""></li>
+            </ul>
+          </div>
+        </div>  
+        <div class="col-lg-2 col-md-6 col-sm-6">
+          <div class="single-footer-widget f_social_wd">
+            <h6 class="footer_title">Follow Us</h6>
+            <p>Let us be social</p>
+            <div class="f_social">
+              <a href="#"><i class="fa fa-facebook"></i></a>
+              <a href="#"><i class="fa fa-twitter"></i></a>
+              <a href="#"><i class="fa fa-dribbble"></i></a>
+              <a href="#"><i class="fa fa-behance"></i></a>
+            </div>
+          </div>
+        </div>            
+      </div>
+      <div class="row footer-bottom d-flex justify-content-between align-items-center">
+        <p class="col-lg-12 footer-text text-center"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+          Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+          <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+        </div>
+      </div>
+    </footer>
+    <!--================ End footer Area  =================-->
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="../../js/jquery-3.2.1.min.js"></script>
+    <script src="../../js/popper.js"></script>
+    <script src="../../js/bootstrap.min.js"></script>
+    <script src="../../js/stellar.js"></script>
+    <script src="../../vendors/lightbox/simpleLightbox.min.js"></script>
+    <script src="../../vendors/nice-select/js/jquery.nice-select.min.js"></script>
+    <script src="../../vendors/isotope/imagesloaded.pkgd.min.js"></script>
+    <script src="../../vendors/isotope/isotope-min.js"></script>
+    <script src="../../vendors/owl-carousel/owl.carousel.min.js"></script>
+    <script src="../../vendors/jquery-ui/jquery-ui.js"></script>
+    <script src="../../js/jquery.ajaxchimp.min.js"></script>
+    <script src="../../js/mail-script.js"></script>
+    <script src="../../js/theme.js"></script>
+
+    <script type="text/javascript">
+      $(document).ready(function(){
+  // Activate tooltip
+  $('[data-toggle="tooltip"]').tooltip();
+  
+  // Select/Deselect checkboxes
+  var checkbox = $('table tbody input[type="checkbox"]');
+  $("#selectAll").click(function(){
+    if(this.checked){
+      checkbox.each(function(){
+        this.checked = true;                        
+      });
+    } else{
+      checkbox.each(function(){
+        this.checked = false;                        
+      });
+    } 
+  });
+  checkbox.click(function(){
+    if(!this.checked){
+      $("#selectAll").prop("checked", false);
+    }
+  });
+});
+</script> 
+</body>
+</html>
---
+++ wedding.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 24 Nov 2018 pada 17.38
+-- Versi server: 10.1.36-MariaDB
+-- Versi PHP: 5.6.38
+ 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+ 
+ 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+ 
+--
+-- Database: `weding`
+--
+ 
+-- --------------------------------------------------------
+ 
+--
+-- Struktur dari tabel `admin`
+--
+ 
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ 
+-- --------------------------------------------------------
+ 
+--
+-- Struktur dari tabel `barang_sewa`
+--
+ 
+CREATE TABLE `barang_sewa` (
+  `id` int(11) NOT NULL,
+  `nama_barang` varchar(30) NOT NULL,
+  `harga_barang` int(50) NOT NULL,
+  `status` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ 
+-- --------------------------------------------------------
+ 
+--
+-- Struktur dari tabel `jasa`
+--
+ 
+CREATE TABLE `jasa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `harga` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ 
+-- --------------------------------------------------------
+ 
+--
+-- Struktur dari tabel `paket`
+--
+ 
+CREATE TABLE `paket` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `harga` int(15) NOT NULL,
+  `status` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ 
+-- --------------------------------------------------------
+ 
+--
+-- Struktur dari tabel `user`
+--
+ 
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `no_hp` varchar(15) NOT NULL,
+  `email` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ 
+--
+-- Indexes for dumped tables
+--
+ 
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+ 
+--
+-- Indeks untuk tabel `jasa`
+--
+ALTER TABLE `jasa`
+  ADD PRIMARY KEY (`id`);
+ 
+--
+-- Indeks untuk tabel `paket`
+--
+ALTER TABLE `paket`
+  ADD PRIMARY KEY (`id`);
+ 
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+ 
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ 
+--
+-- AUTO_INCREMENT untuk tabel `jasa`
+--
+ALTER TABLE `jasa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ 
+--
+-- AUTO_INCREMENT untuk tabel `paket`
+--
+ALTER TABLE `paket`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+ 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
