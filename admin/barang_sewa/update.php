f7ae5968a40da8140c4f361f1ada7e27bdb39fde
febbry ramadhan <ramadhanfebbry@gmail.com>
2018-11-25 11:26:21 +0700

    update

--- admin/barang_sewa/update.php
+++ admin/barang_sewa/update.php
@@ -4,14 +4,14 @@
 	$id = $_GET['id'];
   $nama = $_POST['nama'];
   $harga = $_POST['harga'];
-  
+
   $status = $_POST['status'];
 
   $sql = "UPDATE `barang_sewa` SET `nama_barang` = '$nama',`harga_barang` = '$harga', `status` = '$status'  WHERE `barang_sewa`.`id` = '$id';";
 
   if (mysqli_query($db, $sql)) {
     echo "<SCRIPT type='text/javascript'> 
-              alert('Barang Sewa berhasil diperbaharui.');
+              alert('Barang Sewa berhasil diperbaharui.  ');
               window.location.replace(\"index.php\");
           </SCRIPT>";
   } else {
